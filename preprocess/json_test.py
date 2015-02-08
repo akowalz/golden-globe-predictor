084c101415b56e5f371bc304572b839bb7dee2b2
Alex Kowalczuk <askowalczuk93@gmail.com>
2015-02-08 17:23:59 -0600

    fix encoding

--- preprocess/json_test.py
+++ preprocess/json_test.py
@@ -1,48 +1,50 @@
 import sys
 import json
 import string
+import tweet
+import codecs
 
 def write_tweets(source_path, out_path, attr):
     written = 0
-    with open(source_path, 'r') as json_file:
-        outfile = open(out_path, 'w')
+    with codecs.open(source_path, 'r', 'utf-8') as json_file:
+        outfile = codecs.open(out_path, 'w', 'utf-8')
         for line in json_file:
             content = json.loads(line)[attr]
             content = string.replace(content, "\n", " ")
-            outfile.write(content.encode('utf-8') + "\n".encode('utf-8'))
+            outfile.write(content.encode('utf-8') + "\n")
             written += 1
             if written % 1000 == 0:
                 print "Wrote %d tweets" % written
 
-def search_tweets(source_path, out_path, searchterm_s, arg):	# arg = 0: Search for tweets containing any search terms.
+def search_tweets(source_path, out_path, searchterm_list, arg):	# arg = 0: Search for tweets containing any search terms.
 	count = 0													# arg = 1: Search for tweets containing all search terms.
-	with open(source_path, 'r') as json_file:					# Return number of tweets with positive match.
-		outfile = open(out_path, 'w')
-		for line in json_file:
+	with codecs.open(source_path, 'r', 'utf-8') as f:					# Return number of tweets with positive match.
+		outfile = codecs.open(out_path, 'w', 'utf-8')
+		for line in f:
 			add = "True"
 			content = line
-			tweet_list = content.split()
-			searchterm_list = searchterm_s.split()
+                        t = tweet.Tweet(content)
 			if arg == '0':
-				for element in searchterm_list:
-					if element in tweet_list:
-						count += 1
-						outfile.write(content + "\n".encode('utf-8'))
-						break
-			if arg == '1':
+                                if t.tweet_contains_token_in(searchterm_list):
+                                        count += 1
+                                        if count % 100 == 0:
+                                            print "Wrote %d tweets" % count
+                                        outfile.write(content + "\n")
+			elif arg == '1':
 				for element in searchterm_list:
 					if element not in tweet_list:
 						add = "False"
-						break
+                                                break
 				if add == "True":
 					count += 1
-					outfile.write(content + "\n".encode('utf-8'))
+					outfile.write(content + "\n")
 	return count
 
 def main():		# sys.argv[1] = String of search terms separated by spaces.
 				# sys.argv[2] = 0 or 1 depending on search preferences.
-	#write_tweets("goldenglobes2015.json", "tweets.json", "text")
-	print search_tweets("tweets.json", "searched_tweets.json", sys.argv[1], sys.argv[2])
+	# write_tweets("../data/goldenglobes2015.json", "tweets.txt", "text")
+	print search_tweets("tweets.txt", "best_tweets.txt", ["best", "cecile",
+            "a", "b", "c", "d", "e", "f"], sys.argv[1])
 	return
 
 main()
