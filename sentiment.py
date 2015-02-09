481e96764da7939bcb43ccd000f1a6e878f9b473
Alex Kowalczuk <askowalczuk93@gmail.com>
2015-02-09 15:17:45 -0600

    add sentiment processing

---
+++ sentiment.py
@@ -0,0 +1,29 @@
+from textblob import TextBlob
+import codecs, json
+
+TIME_WINDOW = 60000 # one minute
+
+
+def collect_data(in_path, out_path):
+    sentiments = []
+    outfile = open(out_path, 'w')
+    first_time = 1421014813010
+
+    with codecs.open(in_path, 'r', 'utf-8') as f:
+        for line in f:
+            tweet = json.loads(line)
+            text = tweet["text"]
+            timestamp = int(tweet["timestamp_ms"])
+            sentiment = TextBlob(text).sentiment.polarity
+            sentiments.append(sentiment)
+            if (timestamp - first_time) > TIME_WINDOW:
+                outfile.write("%f,%d\n" % (mean(sentiments), first_time))
+                print "%f,%d" % (mean(sentiments), first_time)
+                sentiments = []
+                timestamps = []
+                first_time = timestamp
+
+def mean(ns):
+    return sum(ns) / float(len(ns))
+
+collect_data("data/goldenglobes2015.json", "sentiments.csv")
