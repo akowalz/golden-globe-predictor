8a194268bc9ee7c2d0f6af0def2f69b446b91570
pommelpro <pommelpro@gmail.com>
2015-02-10 10:35:24 -0600

    sentiment code

---
+++ graph_sent.py
@@ -0,0 +1,22 @@
+import sys
+import csv
+import numpy as np
+import copy
+import matplotlib.pyplot as plt
+
+
+def main():
+	csvfile = open("sentiments.csv", 'rb')
+	dat = csv.reader(csvfile, delimiter=',')
+	column1 = []
+	column2 = []
+	for row in enumerate(dat):
+		column1.append(float(row[1][0]))
+		column2.append(float(row[1][1]))
+	column1 = np.array(column1)
+	column2 = np.array(column2)
+	plt.plot(column1)
+	plt.show()
+	return
+	
+main()
