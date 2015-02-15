c1bf5227c281b185a8f77a7126b94d1c8f5eefc8
{{ Kyle }} <{{ kszela24@gmail.com }}>
2015-02-15 16:25:09 -0600

    Changed it to display time on x-axis

--- graph_sent.py
+++ graph_sent.py
@@ -11,37 +11,15 @@ from datetime import datetime
 def main():
 	csvfile = open("sentiments.csv", 'rb')
 	dat = csv.reader(csvfile, delimiter=',')
-	column1 = []
+	values = []
 	date_list = []
 	for row in enumerate(dat):
-		column1.append(float(row[1][0]))
+		values.append(float(row[1][0]))
 		date_list.append(datetime.fromtimestamp(int(row[1][1])/1000))
-	column1 = np.array(column1)
+	values = np.array(values)
 	dates = matplotlib.dates.date2num(date_list)
-	plt.plot_date(dates, column1, fmt = "o")
+	plt.plot_date(dates, values, fmt = "o")
 	plt.show()
-	#pylab.figure(1)
-	#x = range(len(column2))
-	#pylab.xticks(x, column2)
-	#pylab.plot(x, column1, "g")
-	#pylab.show()
 	return
-
-"""def main():
-	csvfile = open("sentiments.csv", 'rb')
-	dat = csv.reader(csvfile, delimiter=',')
-	column1 = []
-	column2 = []
-	for row in enumerate(dat):
-		column1.append(float(row[1][0]))
-		print time.localtime(int(row[1][1])/1000)
-		column2.append(time.localtime(int(row[1][1])/1000))
-	column1 = np.array(column1)
-	pylab.figure(1)
-	x = range(len(column2))
-	pylab.xticks(x, column2)
-	pylab.plot(x, column1, "g")
-	pylab.show()
-	return"""
 	
 main()
