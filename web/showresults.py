c89a0ed2953c3bbe73a96e78ebf4490788638410
pommelpro <pommelpro@gmail.com>
2015-02-16 17:49:58 -0600

    update web interface

--- web/showresults.py
+++ web/showresults.py
@@ -2,12 +2,15 @@ from flask import Flask, render_template
 import json
 app = Flask(__name__)
 app.debug = True
-with open('../results/winner_round1_percentages.json', 'r') as f:
+with open('../results/winners2015-percents-3.json', 'r') as f:
 	winners = json.loads(f.read())
 
 @app.route('/')
 def root():
 	return render_template('index.html', winners=winners)
+@app.route('/sentiment')
+def sentiment():
+	return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
