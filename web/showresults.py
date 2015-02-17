e6d8e3b1d0b54fe66fe47933f9bd1a6a78647f03
pommelpro <pommelpro@gmail.com>
2015-02-16 18:51:45 -0600

    more web pages

--- web/showresults.py
+++ web/showresults.py
@@ -5,12 +5,18 @@ app.debug = True
 with open('../results/winners2015-percents-3.json', 'r') as f:
 	winners = json.loads(f.read())
 
+pageScroll = 10
+
+
 @app.route('/')
 def root():
-	return render_template('index.html', winners=winners)
+	return render_template('index.html', pageScroll=0)
 @app.route('/sentiment')
 def sentiment():
-	return render_template('index.html')
+	return render_template('index.html', pageScroll=1)
+@app.route('/predictor_winner')
+def predictors():
+	return render_template('index.html', winners=winners)
 
 if __name__ == '__main__':
     app.run()
